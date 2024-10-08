4ad076a8bfa33892eea76af190c2809982e892b8
Prabhdeep Singh <prabhrai94@gmail.com>
2024-10-07 22:07:00 -0400

    commit

--- finos-svc-ts/src/FinosConnector.tsx
+++ finos-svc-ts/src/FinosConnector.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import data from "./data.json";
 import { Channel, DesktopAgent } from "@finos/fdc3";
+import { cwd } from "process";
 
 const FinosConnector = () => {
   const [channel, setChannel] = useState<Channel>();
@@ -60,12 +61,11 @@ const FinosConnector = () => {
         "fdc3.instrument",
         (instrument, metadata) => {
           console.log(
-            `Received context message\nContext: ${JSON.stringify(
-              instrument
-            )}\nOriginating app: ${JSON.stringify(metadata?.source)}`
+            `Received context message\nContext: ${JSON.stringify(instrument)}\nOriginating app: ${JSON.stringify(metadata?.source)}`
           );
           //do something else with the context
           let ticker = instrument?.id?.ticker;
+          
           setCurrentTicker(ticker);
           console.log("received ticker " + ticker);
           var contactResult = fetchContacts(ticker);
@@ -81,7 +81,48 @@ const FinosConnector = () => {
               email: contactResult?.contacts[0]?.email,
             },
           };
-          window.fdc3.raiseIntent("StartChat", contact);
+          // console.log(`raising chat to Symphony - ${JSON.stringify(contact)}`  )
+          // window.fdc3.raiseIntent("StartChat", contact);
+
+
+          // chat with initialization settings
+          const initSettings = {
+            type: 'fdc3.chat.initSettings',
+            chatName: `Deal ${ticker}`,
+            members: {
+                type: 'fdc3.contactList',
+                contacts: [{
+                    type: 'fdc3.contact',
+                    name: contactResult?.contacts[0]?.first_name,
+                    id: {
+                        email: contactResult?.contacts[0]?.email
+                    },
+                }]
+            },
+            options: {
+                groupRecipients: true, // one chat with both contacts
+                public: false, // private chat room
+                allowHistoryBrowsing: true,
+                allowMessageCopy: true,
+                allowAddUser: false, // John won't be authorized to ,add other users to the chat
+            },
+            message: {
+              type: 'fdc3.messgae',
+              text: {
+                "text/markdown": `Deal ${ticker} - Price Offered at ${instrument?.metaData?.price}` 
+              }
+            },
+            initMessage: `Deal ${ticker}`,
+            Comment: `Deal ${ticker}`,
+          }
+
+          console.log(`raising chat to Symphony - ${JSON.stringify(initSettings)}`  )
+          const resolution = window.fdc3.raiseIntent('StartChat', initSettings);
+          let chatRoomRefs;
+          resolution.then( r => r.getResult()).then( res => chatRoomRefs = res )
+          // console.log(chatRoomRefs)
+
+
         }
       );
     } catch (error) {
